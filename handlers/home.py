d848bac7fde99889d09b04682832e52ade0ccf46
ih <irvin.hwang@gmail.com>
2011-11-30 00:37:08 -0800

    added simple template

--- handlers/home.py
+++ handlers/home.py
@@ -1,12 +1,14 @@
 from google.appengine.api import users
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
 
 class HomePage(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
 
         if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Welcome to FocusBell %s!' % user.nickname())
+            template_values = {}
+            self.response.out.write(template.render('templates/home.html', template_values)) #TODO implement template_path
         else:
             self.redirect(users.create_login_url(self.request.uri))
