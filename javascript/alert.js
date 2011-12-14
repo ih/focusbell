16f2dab12f20a2149391ca5efe80f14a9452d8a4
ih <irvin.hwang@gmail.com>
2011-12-14 08:12:35 -0800

    alert comes on with increasing and decreasing interval depending on answer

--- javascript/alert.js
+++ javascript/alert.js
@@ -1,3 +1,22 @@
 $(function(){
-    alert("Focus!");
+    interval = 2000; //TODO make interval a local variable, argument to testFocus?
+    setTimeout(testFocus, interval);
   });
+
+function testFocus(){
+  var focused = confirm("Focused since last time?");
+  if(focused==true){
+    increaseInterval();
+  }
+  else{
+    decreaseInterval();
+  }
+  setTimeout(testFocus, interval);
+}
+function increaseInterval(){
+  interval *= 2;
+}
+
+function decreaseInterval(){
+  interval /= 2;
+}
